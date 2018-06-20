3adab27929ecd297f55562376d1a331f59ad31fe
Veronicah Schenker <veshshenker@gmail.com>
2018-06-20 12:38:42 +0300

    started web server and implemented first end point

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const app = express();
+
+app.listen(3000,()=>{
+    console.log('listening at port 3000');
+})
+
+app.get('/',(req,res)=>{
+    res.status(200).send('this is a git sample');
+})
